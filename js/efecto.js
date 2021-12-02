b66699a48d30aa5cb105dec41cc1a03b3444bd70
Emilio Gril <emiliogril@gmail.com>
2021-12-02 13:36:56 -0300

    nuevo cambio

--- js/efecto.js
+++ js/efecto.js
@@ -7,24 +7,3 @@ $(document).ready( () => {
     visibility: "hidden"})
     }, 1000);
 })
-
-// // Animaciones para el texto
-// $("#textoAnimado").fadeOut("slow", function(){
-//     $("#textoFade").fadeIn(9000);
-// });
-
-// $("#titulo").css("margin-top", "150px")
-// .slideUp(2000)
-// .slideDown(2000);
-
-// //Animacion para la imagen principal
-// $("#quienSoy").fadeIn(2000);
-
-// //Animacion para el texto de debajo
-// $("#textoanimado1").css("margin-top", "150px")
-// .slideUp(2000)
-// .slideDown(2000);
-// $("#textoanimado2")
-// .css("margin-top", "20px")
-// .slideUp(2000)
-// .slideDown(2000);
